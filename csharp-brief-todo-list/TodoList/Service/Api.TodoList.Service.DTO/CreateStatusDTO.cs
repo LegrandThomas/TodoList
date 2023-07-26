abef7bf74ac9fd039bf24ecd14d6df3a19152530
jb-mddln <13810686+jb-mddln@users.noreply.github.com>
2023-07-26 12:13:04 +0200

    Remove unused using

--- csharp-brief-todo-list/TodoList/Service/Api.TodoList.Service.DTO/CreateStatusDTO.cs
+++ csharp-brief-todo-list/TodoList/Service/Api.TodoList.Service.DTO/CreateStatusDTO.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Api.TodoList.Service.DTO
+﻿namespace Api.TodoList.Service.DTO
 {
     public class CreateStatusDTO
     {
-
         public string Value { get; set; } = null!;
     }
 }
--- csharp-brief-todo-list/TodoList/Service/Api.TodoList.Service.DTO/ReadStatusDTO.cs
+++ csharp-brief-todo-list/TodoList/Service/Api.TodoList.Service.DTO/ReadStatusDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Api.TodoList.Service.DTO
+﻿namespace Api.TodoList.Service.DTO
 {
     public class ReadStatusDTO : CreateStatusDTO
     {
